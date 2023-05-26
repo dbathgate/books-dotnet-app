f00df1312c6ff60bd9f14a5e353e5d110e31c6c5
Darren Bathgate <dbathgate@vmware.com>
2023-05-26 15:52:17 -0400

    adding k8s config

--- Program.cs
+++ Program.cs
@@ -1,20 +1,24 @@
 using Microsoft.EntityFrameworkCore;
 using RazorApp.Repository;
 using Steeltoe.Connector.EFCore;
-using Steeltoe.Extensions.Configuration.CloudFoundry;
+using Steeltoe.Extensions.Configuration.Kubernetes;
 using Steeltoe.Connector.MySql.EFCore;
 using Steeltoe.Management.TaskCore;
 using Steeltoe.Common.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.AddCloudFoundryConfiguration();
+builder.AddKubernetesConfiguration();
 builder.UseCloudHosting();
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<BookDbContext>(options => options.UseMySql(builder.Configuration));
 //builder.Services.AddTask<MigrateDbContextTask<BookDbContext>>();
+
+foreach(var config in builder.Configuration.AsEnumerable()) {
+    Console.WriteLine($"{config.Key} = {config.Value}");
+}
 
 var app = builder.Build();
 
